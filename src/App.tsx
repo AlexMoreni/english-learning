3e4501485bb82e69e88e45b2645ea3b92119cd74
AlexMoreni <alex.moreni47@gmail.com>
2023-11-15 16:00:14 -0300

    configuring react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+
+//Pages
+import { Home } from "./pages/Home/Home.tsx";
 
 function App() {
   return (
-    <>
-      <h1>English Learning</h1>
-    </>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

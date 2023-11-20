9e6d9f99fc602e7ac96092832fd6284e7b2d2752
AlexMoreni <alex.moreni47@gmail.com>
2023-11-20 14:37:21 -0300

    creating and styling home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,35 @@
+import styles from "./Home.module.css";
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <div className={styles.container_content}>
+        <img src="illustrations-home.png" alt="" />
+        <div>
+          <h1>Effective English learning for any age</h1>
+          <p>
+            Flexible learning times and materials for all levels will have you
+            speaking with confidence in no time.
+          </p>
+          <button>Get Started</button>
+        </div>
+      </div>
+      <div
+        className={styles.container_content}
+        style={{ justifyContent: "space-around" }}
+      >
+        <div>
+          <h1>Learn English for everyday situations</h1>
+          <p>
+            You will have a complete teaching experience, which will allow you
+            to progress from scratch to fluency in English. With this content,
+            you will feel confident to understand, speak well and be understood
+            anywhere in the world.
+          </p>
+          <button>Start learning</button>
+        </div>
+        <img src="illustration-ads.png" alt="" />
+      </div>
+    </>
+  );
 }

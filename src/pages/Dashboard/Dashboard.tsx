d65373dc2c628d13175246f5a69a752ee79a94eb
AlexMoreni <alex.moreni47@gmail.com>
2023-12-07 18:20:52 -0300

    structuring and beginning the dashboard style

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,3 +1,38 @@
+import styles from "./Dashboard.module.css";
+
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
 export function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (newDate: any) => {
+    setDate(newDate);
+  };
+
+  return (
+    <main>
+      <header className={styles.header}>
+        <div className={styles.cards}>
+          <div>
+            <img src="/writing.png" alt="Lápis roxo" />
+            <h2>Escrever</h2>
+          </div>
+          <div>
+            <img src="/exame.png" alt="Chapéu de formatura" />
+            <h2>Prova</h2>
+          </div>
+        </div>
+        <Calendar
+          className={styles.calendar}
+          onChange={onChange}
+          value={date}
+        />
+      </header>
+      <main className={styles.notes}>
+        <h1>Anotações</h1>
+      </main>
+    </main>
+  );
 }
